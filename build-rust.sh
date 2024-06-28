7b8beea58ef53d0034c323d3354cd67cd2abf3e5
basicallygit <91993321+basicallygit@users.noreply.github.com>
2024-06-28 13:52:13 +0100

    Initial commit

---
+++ build-rust.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git clone https://github.com/basicallygit/$1
+
+mkdir $1-binaries
+
+cd $1
+
+# Build dynamically linked binary
+cargo build --release
+# Build statically linked binary
+cargo build --release --target=x86_64-unknown-linux-musl 
+
+mv target/release/$1 ../$1-binaries/$1
+mv target/x86_64-unknown-linux-musl/release/$1 ../$1-binaries/$1-static
+
+# Strip the binaries of any unnecessary symbols 
+strip ../$1-binaries/$1*
+
+cd ..
+rm -rf $1

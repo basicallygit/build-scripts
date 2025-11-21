0690783e8ed7df0cafab648d4cc5b5d3d860ae74
basicallygit <91993321+basicallygit@users.noreply.github.com>
2025-11-21 16:13:51 +0000

    Rename static binary output to musl

--- build-rust.sh
+++ build-rust.sh
@@ -12,7 +12,7 @@ cargo build --release
 cargo build --release --target=x86_64-unknown-linux-musl 
 
 mv target/release/$1 ../$1-binaries/$1
-mv target/x86_64-unknown-linux-musl/release/$1 ../$1-binaries/$1-static
+mv target/x86_64-unknown-linux-musl/release/$1 ../$1-binaries/$1-musl
 
 # Strip the binaries of any unnecessary symbols 
 strip ../$1-binaries/$1*
